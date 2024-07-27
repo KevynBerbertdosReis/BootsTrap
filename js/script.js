bb14a75d13d97ec2bce1eee5daa1b1231d76012c
Kevyn <dosreiskevyn@gmail.com>
2024-07-27 12:08:30 -0300

    arrays e objetos

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+let productsArray = [];
+
 function add() {
     let inputProduto = document.getElementById('inputProduto').value;
     let inputMarca = document.getElementById('inputMarca').value;
@@ -6,13 +9,16 @@ function add() {
     let inputVendas = document.getElementById('inputVendas').value;
 
     const dataProduct = {
-        id: Math.random(),
+        
+   id: Math.random(),
         produto: inputProduto,
         marca: inputMarca,
         estoque: inputEstoque,
         custo: inputCusto,
         vendas: inputVendas,
-    }
-
-    console.log(dataProduct);
 }
+    productsArray.push(dataProduct)
+    console.log(productsArray);
+
+}
+
