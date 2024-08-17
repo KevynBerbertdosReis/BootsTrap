6d3a8c4b716b1331b984f389fc282d7f6ced88b1
Kevyn <dosreiskevyn@gmail.com>
2024-08-17 12:16:29 -0300

    adicionando uma array

---
+++ Aula 10-08-24/js/laco-repeticao.js
@@ -0,0 +1,15 @@
+
+let typeUser = "Admin";
+
+if (name === "User") {
+  
+} else {
+}
+
+for (let x = 1; x <= 10; x++) {
+  if (x >= 2 || x < 10) {
+    console.log("IF", x);
+  } else {
+    console.log("ELSE", x);
+  }
+}
